b1f001a3c8fda07382ae234f092d171c88678359
Kris The Crazie <kriswattie@gmail.com>
2025-10-02 09:45:24 -0400

    Update ipPhoneclear.ps1
    Fixing the script for a cleaner output

--- Aria Automation Scripts/AD/ipPhoneclear.ps1
+++ Aria Automation Scripts/AD/ipPhoneclear.ps1
@@ -4,26 +4,23 @@ $inactiveOUs = Get-ADOrganizationalUnit -Filter 'Name -like "Inactive*"' -Proper
 # Loop through each inactive OUs
 foreach ($ou in $inactiveOUs) {
     $ouDN = $($ou.DistinguishedName)
-    Write-Host "Processing OU: $($ou.Name)"
+    $parentOU = ($ou.DistinguisedName -split ',')[2] -replace '^OU=', ''
+
+    Write-Host " --- OU: $parentOU Processing --- "
     
     # Get All user accounts within the current OU and its sub-OUs
-    $usersinOU = Get-ADUser -Filter * -SearchBase $ouDN -Properties ipPhone
+    $usersinOU = Get-ADUser -Filter {ipPhone -like "*"} -SearchBase $ouDN -Properties ipPhone
     
     # Loop through each user
-    foreach ($user in $usersinOU)
+    foreach ($user in $usersinOU) {
         $usersam = $($user.SamAccountName)
         $userdn = $($user.DistringuisedName)
-        Write-Host "Checking ipPhone attribute for user: $userdn"
-        
-        # Check if IP attribute is already clear
-        if ([string]::IsNullOrEmpty($($user.ipPhone))) {
-            Write-Host "ipPhone attribute is already clear for user $usersam"
-        } 
-        else {
-            Write-Host "Clearing ipPhone attribute for user: $usersam"
-            # Clear the ipPhone attribute
-            Set-ADUser -Identity $userDN -Clear ipPhone
-        }
+        Write-Host "Clearing ipPhone attribute for user: $userSam"
+
+        # Clear the ipPhone Attribute
+        Set-ADUser -Identity $userDN -Clear ipPhone
     }
+    Write-Host " --- OU: $parentOU Completed --- "
 }
+
 Write-Host "Completed clearing ipPhone attribute for all users in inactive OUs."
