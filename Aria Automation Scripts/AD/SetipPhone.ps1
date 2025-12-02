f23acd70ee972eb11c45cf79996ba404cbc924ab
Kris The Crazie <kriswattie@gmail.com>
2025-12-02 09:37:10 -0500

    Create SetipPhone.ps1

---
+++ Aria Automation Scripts/AD/SetipPhone.ps1
@@ -0,0 +1,47 @@
+# Define your OU-specific thresholds (in days)
+# Add as many OU's as required
+$OUThresholds = @{
+    "OU=1,OU=Admins,DC=default,DC=ca" = 30,
+    "OU=2,OU=Admins,DC=default,DC=ca" = 30
+}    
+$logpath = "C:\Temp\InactiveAdmin.log"
+if (-not (Test-Path "C:\Temp")) {
+    New-Item -Path "C:\Temp" -ItemType Directory | Out-Null
+}
+
+# Clear the Log content at the beginning
+Clear-Content -Path $logpath -ErrorAction SilentlyContinue
+Add-Content -Path $logpath -Value "=== Stale Admin Cleanup Started - $(Get-Date) ===`r`n"
+
+foreach ($OU in $OUThresholds.Key) {
+    $CreatedDays = 1
+    $CutoffDate = (Get-Date).AddDays(-$CreatedDays)
+    
+    # Extract the OU Name
+    $OUName = ($OU -split ',')[O] -replace '^OU=',''
+    
+    $sectionHeader = @(
+        "==================================================",
+        "OU: $OUName",
+        "Users Recently Created",
+        "=================================================="
+    )
+    
+    $sectionHeaderText = $sectionHeader -join "`r`n"
+    Write-Host $sectionHeaderText
+    Add-Content -Path $logpath -Value "$sectionHeaderText`r`n"
+    
+    # Get Users who are enabled and haven't logged in since cutoff
+    $Users = Get-ADUser =SearchBase $OU -Filter { Enabled -eq $true -and whenCreated -lt $CutoffDate } -Properties SamAccountName, whenCreated, lastLogonDate
+    if ($Users) {
+        foreach ($User in $Users) {
+            # Searching for users that have been inactive based on the threshold that has been set
+            if ($User.ipPhone -eq $null){
+                $userDN = $($User.DistinguishedName)
+                $line = "$($User.SamAccountName) - created  $($User.whenCreated)"
+                Set-ADUser -Identity $userDN -Replace @{ipPhone = "2"}
+                Write-Host $line
+                Add-Content -Path $LogPath -Value "$line`r`n"
+        }
+    }
+}
